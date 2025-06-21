e307efdaa12f124c496d695bb17a21a8869e99f3
unknown <shannu0216@gmail.com>
2025-06-21 13:05:48 +0530

    fixed Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
+import Footer from "@/components/ui/Footer";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
