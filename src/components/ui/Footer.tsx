c56a184f8e621e00d5e125b7c7ac67d023614b69
SHANMUKH SRINIVAS <shannu0216@gmail.com>
2026-03-26 19:07:32 +0530

    Update Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="text-center text-sm text-gray-500 py-4 bg-white border-t mt-10">
       <p>
         Made by: <strong>Seeram Shanmukh Srinivas</strong>,{" "}
-        <strong>Akash N</strong>, <strong>Nikitha N</strong>
+        <strong>Akash N</strong>
       </p>
     </footer>
   );
